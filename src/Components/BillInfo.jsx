73bdfdb783d98222ea0354e4c908107b61d6de29
PRIYAM <you@example.com>
2024-10-21 23:17:39 +0530

    adding pdf download functionality

--- src/Components/BillInfo.jsx
+++ src/Components/BillInfo.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import InvoiceDetails from "./InvoiceDetails";
 import DateInvoiceGST from "./DateInvoiceGST";
 import ItemTable from "./ItemTable";
 import TotalAmount from "./TotalAmount";
 import PreviewPdfButton from "./PreviewPdfButton";
 import DocumentPreview from "./DocumentPreview";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+import html2pdf from 'html2pdf.js';
 
 export default function BillInfo() {
   const [items, setItems] = useState([
@@ -38,9 +41,39 @@ export default function BillInfo() {
   const totalAmount = items.reduce((acc, cur) => acc + parseFloat(cur.amount || 0), 0);
   const totalGst = items.reduce((acc, cur) => acc + parseFloat(cur.cgstAmount || 0) + parseFloat(cur.utgstAmount || 0) + parseFloat(cur.igstAmount || 0), 0);
 
-  const [invoiceNumber, setInvoiceNumber] = useState();
+  const [invoiceNumber, setInvoiceNumber] = useState(1);
   const [invoiceDate, setInvoiceDate] = useState("2024-01-01");
   const [gstNum, setGstNum] = useState();
+
+  const printRef = useRef();
+
+  // const handleDownloadPdf = async () => {
+  //   const element = printRef.current;
+  //   const canvas = await html2canvas(element, { scale: 2 });  // scale: 2 for high-quality PDF
+  //   const data = canvas.toDataURL("image/png");
+  //   const pdf = new jsPDF("p", "mm", "a4");
+    
+  //   const imgProps = pdf.getImageProperties(data);
+  //   const pdfWidth = pdf.internal.pageSize.getWidth();
+  //   const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+
+  //   pdf.addImage(data, "PNG", 0, 0, pdfWidth, pdfHeight);
+  //   pdf.save(`${invoiceNumber}Invoice.pdf`);
+  // };
+
+  const handleDownloadPdf = () => {
+    const element = printRef.current;
+    
+    const options = {
+      margin: 1,
+      filename: 'invoice.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    };
+  
+    html2pdf().set(options).from(element).save();
+  };
 
   return (
     <div className="invoice-details">
@@ -49,7 +82,9 @@ export default function BillInfo() {
       <ItemTable items={items} setItems={setItems} />
       <TotalAmount totalAmount={totalAmount} totalGst={totalGst} />
       <PreviewPdfButton />
-      <DocumentPreview items={items} details={details} date={invoiceDate} invoice={invoiceNumber} gst={gstNum} totalAmount={totalAmount} totalGst={totalGst}/>
+      <div ref={printRef}><DocumentPreview items={items} details={details} date={invoiceDate} invoice={invoiceNumber} gst={gstNum} totalAmount={totalAmount} totalGst={totalGst}/>
+      </div>
+      <button onClick={handleDownloadPdf}>Download PDF</button>
     </div>
   );
 }
