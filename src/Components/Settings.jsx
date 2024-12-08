b29ba260299840117afaf4a0f45bd1e540f80071
PRIYAM <you@example.com>
2024-12-08 15:13:44 +0530

    added dark mode

--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -4,7 +4,7 @@ import {useTheme} from '../Contexts/ThemeContext'
 export default function Settings() {
     const {isDark, handleTheme} = useTheme();
   return (
-    <div>
+    <div className={`settings ${isDark ? "dark" : ""}`}>
         <button onClick={handleTheme}>{isDark ? "Light" : "Dark"} Mode</button>
     </div>
   )
